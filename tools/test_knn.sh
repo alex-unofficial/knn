48fba8ea1ac9328ef5951d42227d99d6a115c89c
Alexandros Athanasiadis <athanasiadisalex2@gmail.com>
2023-01-08 23:51:37 +0200

    changed batch files to include julia module

--- tools/test_knn.sh
+++ tools/test_knn.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=128
 
-module load gcc/8.2.0 openmpi/4.1.2-6ojeowp gsl/2.5 pkgconf/1.7.3
+module load gcc/10.2.0 openmpi/4.0.5 gsl/2.5 pkgconf/1.7.3 julia/1.6.3
 
 make clean && make ./bin/test_knn
 
--- tools/test_ring.sh
+++ tools/test_ring.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=128
 
-module load gcc/8.2.0 openmpi/4.1.2-6ojeowp gsl/2.5 pkgconf/1.7.3
+module load gcc/10.2.0 openmpi/4.0.5 gsl/2.5 pkgconf/1.7.3 julia/1.6.3
 
 make clean && make
 srun ./bin/test_ring
