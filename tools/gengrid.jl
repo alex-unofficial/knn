50a06a569e17e55aeeac565398918cf76c54e3ea
Alexandros Athanasiadis <athanasiadisalex2@gmail.com>
2023-01-09 00:05:57 +0200

    modified gengrid

--- tools/gengrid.jl
+++ tools/gengrid.jl
@@ -1,12 +1,19 @@
+#!/bin/bash
+#=
+exec julia --color=yes --startup-file=no "${BASH_SOURCE[0]}" "$@"
+=#
+
+filename = ARGS[1];
 dim = parse(UInt32, ARGS[2]);
 side = parse(UInt32, ARGS[3]);
 
-@assert dim >= 1 ("dim (number of dimensions) must be a positive integer")
-@assert side >= 2 ("side (number of points in each side) must be at least 2")
+@assert isdir(dirname(filename)) ("directory $(dirname(filename)) does not exist.")
+@assert dim >= 1 ("dim (number of dimensions) must be a positive integer.")
+@assert side >= 2 ("side (number of points in each side) must be at least 2.")
 
 rows = Iterators.product((0:(side - 1) for _ in 1:dim)...);
 
-open(ARGS[1], "w") do file
+open(filename, "w") do file
     for row in rows
         line = strip(foldl((l, r) -> l * "$r ", row; init=""), [' ']) * '\n';
         write(file, line)
