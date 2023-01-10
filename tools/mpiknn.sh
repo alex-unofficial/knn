66809d7c0b291e6d786ad93fb1b5254c8f25f752
Alexandros Athanasiadis <athanasiadisalex2@gmail.com>
2023-01-11 01:41:10 +0200

    minor changes on hpc

--- tools/mpiknn.sh
+++ tools/mpiknn.sh
@@ -2,11 +2,12 @@
 #SBATCH --partition=rome
 #SBATCH --time=1:00:00
 #SBATCH --job-name=mpiknn
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=128
+#SBATCH --nodes=3
+#SBATCH --ntasks-per-node=3
+#SBATCH --cpus-per-task=10
+#SBARCH --mem=5G
 
-module load gcc/10.2.0 openmpi/4.0.5 gsl/2.5 pkgconf/1.7.3 julia/1.6.3
+module load gcc/10.2.0 openmpi/4.0.5 gsl/2.5 pkgconf/1.7.3
 
 make --quiet clean && make --quiet
 
