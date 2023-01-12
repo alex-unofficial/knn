d9babb33e224f6c0e9b1dc70a01e842826ab8540
Alexandros Athanasiadis <athanasiadisalex2@gmail.com>
2023-01-13 00:16:36 +0200

    changes to tests

--- tools/mpiknn.sh
+++ tools/mpiknn.sh
@@ -2,14 +2,17 @@
 #SBATCH --partition=rome
 #SBATCH --time=1:00:00
 #SBATCH --job-name=mpiknn
-#SBATCH --nodes=3
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=128
-#SBATCH --mem=250G
+#SBATCH --mem=0
 
 module load gcc/10.2.0 openmpi/4.0.5 gsl/2.5 pkgconf/1.7.3
 
-make --quiet clean && make --quiet
+make
+
+OUTFILE=data/results/knn_$SLURM_JOB_ID.out
+LOGFILE=data/results/knn.log
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-srun ./bin/mpiknn -i $1 -o knn_$SLURM_JOB_ID.out -l knn.log -m 250G -k $2
+srun ./bin/mpiknn -i $1 -k $2 -o $OUTFILE -l $LOGFILE -m $3
