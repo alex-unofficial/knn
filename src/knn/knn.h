309d93ab900b0d45fb73d6755dbb105040d155b4
Alexandros Athanasiadis <athanasiadisalex2@gmail.com>
2023-01-15 05:56:30 +0200

    fixed indenting error with licence

--- src/knn/knn.h
+++ src/knn/knn.h
@@ -1,20 +1,20 @@
-/*  knn.h - defines the knn_result struct and function declerations
- *  Copyright (C) 2023  Alexandros Athanasiadis
+/* knn.h - defines the knn_result struct and function declerations
+ * Copyright (C) 2023  Alexandros Athanasiadis
  *
- *  This file is part of knn
+ * This file is part of knn
  *
- *  knn is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * knn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  knn is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * knn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef KNN_H
--- tests/test_qselect.c
+++ tests/test_qselect.c
@@ -1,20 +1,20 @@
-/*  test_qselect - test for the qselect method for various input arrays
- *  Copyright (C) 2023  Alexandros Athanasiadis
+/* test_qselect - test for the qselect method for various input arrays
+ * Copyright (C) 2023  Alexandros Athanasiadis
  *
- *  This file is part of knn
+ * This file is part of knn
  *
- *  knn is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * knn is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  knn is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * knn is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <stdio.h>
